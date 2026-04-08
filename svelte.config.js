f25738d07fbab3258553c0f80dc956162ff659de
revfanc <revfanc@icloud.com>
2026-04-08 17:26:38 +0800

    fix: adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-node'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,12 +7,7 @@ const config = {
     // eslint-disable-next-line e18e/prefer-static-regex
     runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
   },
-  kit: {
-    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
-  },
+  kit: { adapter: adapter() },
 }
 
 export default config
