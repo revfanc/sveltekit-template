fa44cefb6b62b53190ca67524a87e402b53612fa
revfanc <revfanc@icloud.com>
2026-04-09 08:56:19 +0800

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import tailwindcss from '@tailwindcss/vite'
-import legacy from '@vitejs/plugin-legacy'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
-    legacy({
-      targets: ['ie >= 11'],
-      modernPolyfills: ['es.array.flat-map', 'es.array.find-last'],
-    }),
     tailwindcss(),
     sveltekit(),
   ],
